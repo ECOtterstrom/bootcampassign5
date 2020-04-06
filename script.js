0b154eb6e0c5da09adf3c44473c3f058e771c9f2
Erin Otterstrom <hapiotter@gmail.com>
2020-04-05 19:47:33 -0600

    items saved in local storage

--- script.js
+++ script.js
@@ -11,15 +11,9 @@
 // let table1 = document.createElement('table') Javascript
 // let table1 = document.querySelector('table') Javascript
 
-//Variables
-// var dayStart
-// var hourNum = $("#hourNum");
-// var taskText = $("#taskText");
-// var saveBtn = $("#saveBtn");
-
 //Array of hours
 var hours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM","3PM", "4PM", "5PM"];
-
+var tr, hourNum, taskText, saveBtn, taskInput, save, i
 
 
 //Function to create table
@@ -27,15 +21,15 @@ function createTable() {
   
 
     for (var i=0; i < hours.length; i++){
-      let tr = $('<tr>');
-      let hourNum = $('<td>');
-      let taskText = $('<td>');
-      let saveBtn = $('<td>');
+      tr = $('<tr>');
+      hourNum = $('<td>');
+      taskText = $('<td>');
+      saveBtn = $('<td>');
      
       hourNum.text(hours[i]);
-      var taskInput = $('<textarea id="task" class = "input" rows="2" cols="115">');
+      taskInput = $('<textarea id="task" class = "input" rows="2" cols="115">');
       taskText.append(taskInput);
-      var save = $('<button type="button" class="saveBtn btn-lg"><i class="far fa-save"></i></button>');
+      save = $('<button type="button" class="saveBtn btn-lg"><i class="far fa-save"></i></button>');
       saveBtn.append(save);
       tr.append(hourNum, taskText, saveBtn);
       $("#dayPlan").append(tr);
@@ -43,46 +37,24 @@ function createTable() {
   };
   createTable();
 
+  //Function to clear local storage before adding new items  
+  function deleteItems() {
+    localStorage.clear();
+  }
+  deleteItems();
+    
   //Function to save inputs to local storage
   $('.saveBtn').click(function(){
-    // alert('Click!')
-    $('.input').each(function(){
-      alert($(this).val())
+      
+  $('textarea').each(function(){
+    var entry, value;
+    entry = $(this);
+    value = (entry.val()); 
+    localStorage.setItem(value, value);
+    //alert($(this).val())
     });
   });
-console.log(task);
-  // $('.input').click(function(){
-  //   alert('Click2!')
-  // })
-  
-
-  // $(document).ready(function(){
-  //   $('saveBtn').on('click', function(){
-  //     alert('Click!')
-  //     // $('input[type="textarea"]').each(function(){    
-  //     //     var id = $("textarea#task[i]").attr('id');
-  //     //     var value = $("textarea#task[i]").val();
-  //     //    localStorage.setItem(id, value);
-         
-  //       //  if (localStorage.getItem("textarea#task[0]") === null) {
-  //       //   console.log("not there!")
-  //       // } else {
-  //       //   console.log("added")
-  //       // }   
-  //     });
-  //   });
-  // });
-
-  
-
-
-
-
-
-
-
-  // console.log(task);
-
+    console.log(localStorage);
 
   //Function to change colors of each row based on time (past, present, future)
   
@@ -94,27 +66,3 @@ console.log(task);
     $('#currentDay').append(date); 
   }
   currentDate();
-
-
-//Create row with hours col, task col, save button
-//Use for loop on the hours array to put one of the hours in the hours col, then create new rows with each subsequent hour
-
-{/* <h3>A demonstration of how to access a TEXTAREA element</h3>
-
-Address:<br>
-<textarea id="myTextarea">
-342 Alvin Road
-Ducksburg</textarea>
-
-<p>Click the button to get the content of the text area.</p>
-
-<button type="button" onclick="myFunction()">Try it</button>
-
-<p id="demo"></p>
-
-<script>
-function myFunction() {
-  var x = document.getElementById("myTextarea").value;
-  document.getElementById("demo").innerHTML = x;
-}
-</script> */}
